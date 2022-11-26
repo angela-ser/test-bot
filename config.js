4abf58555681ce2ed3b40c176560706d4c658b86
Angela <104827062+angela-ser@users.noreply.github.com>
2022-11-26 19:26:09 +0330

    Update config.js

--- config.js
+++ config.js
@@ -56,5 +56,5 @@ module.exports = {
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
   OWNER_NAME: process.env.OWNER_NAME || "HyNO",
   BOT_NAME: process.env.BOT_NAME || "HyNO-MD",
-  WORK_TYPE: process.env.WORK_TYPE || "public",
+  WORK_TYPE: process.env.WORK_TYPE || "private",
 };
